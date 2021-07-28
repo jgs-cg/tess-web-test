b7d24dad735328aeb1cec2b8aedfd03d061c42b5
Jay Batra <Jay_Singh@capgroup.com>
2021-07-28 15:35:23 -0700

    added option to make chrome headless

--- src/test/java/com/capgroup/tess/tess_web_auto/RunCucumberTestNgTest.java
+++ src/test/java/com/capgroup/tess/tess_web_auto/RunCucumberTestNgTest.java
@@ -16,7 +16,7 @@ import org.testng.annotations.DataProvider;
 public class RunCucumberTestNgTest extends AbstractTestNGCucumberTests {
 
         @Override
-        @DataProvider(parallel = false)
+        @DataProvider(parallel = true)
         public Object[][] scenarios() {
                 return super.scenarios();
         }
