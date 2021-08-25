d031ff4b94c6f0cd7dfc30ae47ae4d527b65119c
jgs-cg <78980060+jgs-cg@users.noreply.github.com>
2021-08-24 17:49:00 -0700

    Update uploadResultsQtest.py

--- uploadResultsQtest.py
+++ uploadResultsQtest.py
@@ -12,7 +12,7 @@ cycleId =os.environ['cycleId']
 requestedParser =os.environ['requestedParser']
 contactInitials = os.environ['contactInitials']
 env = os.environ['environment']
-url = os.environ['WEB_HOOK']
+url = "https://qtest.cguser.capgroup.com:4080/webhook/ad6396c2-c0ca-4123-860a-681f47b56af1"
 
 with open(filePath + "/" + fileName, "r") as read_file:
      data = read_file.read()
@@ -26,8 +26,7 @@ payload = {"result": data ,
            "requestedParser" : requestedParser,
            "jobUrl" : "jobUrl",
            "contactInitials" : contactInitials,
-           "modulePath": env,
-           "url": "https://qtest.cguser.capgroup.com:4080/webhook/ad6396c2-c0ca-4123-860a-681f47b56af1"
+           "modulePath": env
            }
 
 headerInfo = {'Content-Type' : 'application/json'}
